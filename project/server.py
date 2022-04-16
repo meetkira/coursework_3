5c75286923fe543cca46ac7c9c9036230e1eefe5
Кристи <kristi.2001@yandex.ru>
2022-04-16 14:33:11 +0300

    Init commit

---
+++ project/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+from flask_restx import Api
+
+from project.setup_db import db
+from project.views import genres_ns
+
+api = Api(
+    authorizations={
+        "Bearer": {"type": "apiKey", "in": "header", "name": "Authorization"}
+    },
+    title="Flask Course Project 3",
+    doc="/docs",
+)
+
+
+def create_app(config_obj):
+    app = Flask(__name__)
+    app.config.from_object(config_obj)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    db.init_app(app)
+    api.init_app(app)
+
+    # Регистрация эндпоинтов
+    api.add_namespace(genres_ns)
+    # api.add_namespace(directors_ns)
+    # api.add_namespace(movies_ns)
+
+    return app
